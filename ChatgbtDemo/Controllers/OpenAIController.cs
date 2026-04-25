a26fb1965fbaca6b9b0b9777783527e4f658f813
Subhan <subhanshh@code.edu.az>
2026-04-26 03:13:51 +0400

    added project

---
+++ ChatgbtDemo/Controllers/OpenAIController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenAI.Chat;
+
+namespace ChatgbtDemo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class OpenAIController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public OpenAIController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpPost]
+        public async Task<IActionResult> GetAIBasedResult(string prompt)
+        {
+            if (string.IsNullOrWhiteSpace(prompt))
+                return BadRequest();
+
+            string APIKey = _configuration["OpenAI:SecretKey"];
+            string answer = string.Empty;
+
+            string modelName = "gpt-4o";
+            var client = new ChatClient(modelName, APIKey);
+
+            var messages = new List<ChatMessage>();
+
+            messages.Add(new UserChatMessage(prompt));
+                var response = await client.CompleteChatAsync(messages);
+            return Ok(response.Value.Content[0].Text);
+        }
+    }
+}
